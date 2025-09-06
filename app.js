06d637e380cff9203650c6e9a405dad6cf969938
gobirg3108 <gobigobi3108@gmail.com>
2025-09-06 15:52:20 +0530

    feat(auth): add login and register functionality

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import adminRoutes from "./routes/admin/adminRoutes.js";
 
 
-dotenv.config()
+const app = express();
 
-const app = express()
+// Middleware
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-// Mongo DB Connection
+// Routes
+
+app.use("/admin", adminRoutes);
+
+// MongoDB Connection
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Mongo DB Connected ✔"))
-    .catch((err) => console.error("Mongo DB Connection Failed", err)
-)
-    
-//Server Connection
-app.listen(process.env.PORT, () => {
-    console.log(`Server Running on ${process.env.PORT}`);
+    .catch((err) => console.error("Mongo DB Connection Failed ", err));
 
-})
+// Server Connection
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server Running on ${process.env.PORT} `);
+});
---
+++ controllers/admin/adminAuth.js
@@ -0,0 +1,58 @@
+import dotenv from "dotenv";
+dotenv.config();
+import Admin from "../../models/admin/adminSchema.js";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+};
+
+// Admin Register
+
+export const adminRegister = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const existingAdmin = await Admin.findOne({ email });
+
+        if (existingAdmin) {
+            return res.status(400).json({ message: "Admin already exists" });
+        }
+
+        const admin = await Admin.create({ email, password });
+
+        const token = generateToken(admin._id);
+
+        res.status(201).json({ token, admin: { id: admin._id, email: admin.email }, message: "Admin created successfully" });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Admin Login
+
+export const adminLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const admin = await Admin.findOne({ email }).select("+password");
+
+        console.log("admin", admin);
+
+        if (!admin || !(await admin.comparePassword(password))) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const token = generateToken(admin._id)
+
+        res.json({
+            token,
+            message: "Login successful",
+            admin: { id: admin._id, email: admin.email }
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
