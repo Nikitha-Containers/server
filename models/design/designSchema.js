7e20304d592b090d5b5f0ccbe8f2e81c39eee306
gobirg3108 <gobigobi3108@gmail.com>
2025-12-06 19:53:44 +0530

    Design Router Update is Good

--- models/design/designSchema.js
+++ models/design/designSchema.js
@@ -4,14 +4,13 @@ const designSchema = new mongoose.Schema(
   {
     saleorder_no: { type: String, required: true, unique: true },
     posting_date: { type: Date },
+    quantity: { type: String, default: "" },
     machine: { type: String, default: "" },
-    totalQty: { type: String, default: "" },
     components: { type: Object, default: {} },
     art_work: { type: String, default: "" },
-    size: { type: String, default: "" },
+    item_description: { type: String, default: "" },
     customer_name: { type: String, default: "" },
-    start_date: { type: Date },
-    end_date: { type: Date },
+    due_date: { type: Date },
   },
   { timestamps: true }
 );
